488f70e339cc38e4d6012283588ccc1f33227145
Mario Borizovski <makomakoku02@gmail.com>
2026-04-09 18:22:12 +0200

    Configuring CORS for web development

--- ManagementPropertiesApplication-master/ManagementProject/src/main/java/org/example/managementproject/config/WebConfig.java
+++ ManagementPropertiesApplication-master/ManagementProject/src/main/java/org/example/managementproject/config/WebConfig.java
@@ -27,6 +27,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .setCachePeriod(0);
     }
 
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
